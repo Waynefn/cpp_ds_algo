09f209b3d10bde7ed54483f389d12ffba8eb24c1
we333 <wantone321@gmail.com>
2018-01-08 16:30:53 +0900

    dp subset doing

--- dp.cpp
+++ dp.cpp
@@ -15,6 +15,7 @@ using namespace std;
 		2.1 LCS(Longest common subsequence)
 	3.区间动态规划 interval dp
 		3.1 组成回文串的最小添加字符数
+	4.子集和(subset sum)
 	4.01背包
 
 */
@@ -26,12 +27,12 @@ void sum_n(int n)
 {
 	PRINT_SUB_FUNCTION_NAME;
 
-	int f[20] = {0};
-	f[0] = f[1] = f[2] = 1; f[3] = 2;
+	int d[20] = {0};
+	d[0] = d[1] = d[2] = 1; d[3] = 2;
 
 	for(int i = 4; i <= n; i++)
-		f[i] = f[i-1] + f[i-3] + f[i-4];
-	cout<<"f["<<n<<"] = "<<f[n]<<endl;
+		d[i] = d[i-1] + d[i-3] + d[i-4];
+	cout<<"d["<<n<<"] = "<<d[n]<<endl;
 }
 
 /**********************************************
@@ -66,7 +67,7 @@ void test_1_dim()
 		LCS = 4 -> BCBA,BCAB,BDAB
 	** 尚未引入flag[][]，无法正确显示所有LCS结果
 **********************************************/
-void longest_common_subsequence_result(int f[MAX][MAX], char x[], int i, char y[], int j)
+void longest_common_subsequence_result(int d[MAX][MAX], char x[], int i, char y[], int j)
 {
 	while(i && j)
 	{
@@ -77,14 +78,14 @@ void longest_common_subsequence_result(int f[MAX][MAX], char x[], int i, char y[
 		}
 		else
 		{
-			if(f[i-1][j] > f[i][j-1])		// 找到相对大的上一层子问题
+			if(d[i-1][j] > d[i][j-1])		// 找到相对大的上一层子问题
 				i--;
-			else if(f[i-1][j] < f[i][j-1])	// 找到相对大的上一层子问题
+			else if(d[i-1][j] < d[i][j-1])	// 找到相对大的上一层子问题
 				j--;
 			else							// 两个子问题相等，则递归去寻找所有可能性
 			{
-				longest_common_subsequence_result(f, x, i-1, y, j);
-				longest_common_subsequence_result(f, x, i, y, j-1);
+				longest_common_subsequence_result(d, x, i-1, y, j);
+				longest_common_subsequence_result(d, x, i, y, j-1);
 				return;
 			}
 		}
@@ -97,26 +98,26 @@ void longest_common_subsequence(char x[], char y[])
 
 	int len_x = strlen(x);
 	int len_y = strlen(y);
-	int f[MAX][MAX];
+	int d[MAX][MAX];
 
 	for(int i = 0; i <= len_x; i++)
-		f[i][0] = 0;
+		d[i][0] = 0;
 	for(int j = 0; j <= len_x; j++)
-		f[0][j] = 0;
+		d[0][j] = 0;
 
 	// [i][0]和[0][j]是留给初始状态的，计算f时i和j要从下标1开始，但字符串首字符下标为x[0]和y[0]
 	for(int i = 1; i <= len_x; i++)
 		for(int j = 1; j <= len_y; j++)
-			if(x[i-1] == y[j-1])	f[i][j] = f[i-1][j-1] + 1;
-			else					f[i][j] = max(f[i-1][j], f[i][j-1]);
+			if(x[i-1] == y[j-1])	d[i][j] = d[i-1][j-1] + 1;
+			else					d[i][j] = max(d[i-1][j], d[i][j-1]);
 
-	cout<<"LCS["<<x<<"]["<<y<<"] = "<<f[len_x][len_y]<<endl;
+	cout<<"LCS["<<x<<"]["<<y<<"] = "<<d[len_x][len_y]<<endl;
 
 #ifdef DEBUG_LOG
-	PRINT_MATRIX(f, len_x+1, len_y+1);
+	PRINT_MATRIX(d, len_x+1, len_y+1);
 #endif
 
-	longest_common_subsequence_result(f, x, len_x, y, len_y); cout<<endl;
+	longest_common_subsequence_result(d, x, len_x, y, len_y); cout<<endl;
 }
 
 /**********************************************
@@ -125,7 +126,7 @@ void longest_common_subsequence(char x[], char y[])
 		y:ABFCAB
 		LCS = 2 -> AB
 **********************************************/
-void longest_common_substring_result(int f[MAX][MAX], char x[], char y[], int p, int q)
+void longest_common_substring_result(int d[MAX][MAX], char x[], char y[], int p, int q)
 {
 	stack<char> ret;
 
@@ -153,13 +154,13 @@ void longest_common_substring(char x[], char y[])
 
 	int len_x = strlen(x);
 	int len_y = strlen(y);
-	int f[MAX][MAX];
+	int d[MAX][MAX];
 	int p,q;
 
 	for(int i = 0; i <= len_x; i++)
-		f[i][0] = 0;
+		d[i][0] = 0;
 	for(int j = 0; j <= len_y; j++)
-		f[0][j] = 0;
+		d[0][j] = 0;
 
 	int curr_max = -1;
 	for(int i = 1; i <= len_x; i++)
@@ -167,19 +168,19 @@ void longest_common_substring(char x[], char y[])
 		for(int j = 1; j <= len_y; j++)
 		{
 			if(x[i-1] == y[j-1])
-				f[i][j] = f[i-1][j-1] + 1;
+				d[i][j] = d[i-1][j-1] + 1;
 			else
-				f[i][j] = 0;	// 一旦不匹配就归零，接下来重新计算
-			if(f[i][j] > curr_max)
+				d[i][j] = 0;	// 一旦不匹配就归零，接下来重新计算
+			if(d[i][j] > curr_max)
 			{
-				curr_max = f[i][j];
+				curr_max = d[i][j];
 				p = i, q = j;	// 记录最长子串的末尾，输出时往前倒推即可得到结果
 			}
 		}
 	}
 	cout<<"LCS["<<x<<"]["<<y<<"] = "<<curr_max<<endl;
 
-	longest_common_substring_result(f, x, y, p, q);
+	longest_common_substring_result(d, x, y, p, q);
 }
 
 void test_2_dim()
@@ -196,12 +197,12 @@ void create_palindrome(char str[])
 	PRINT_SUB_FUNCTION_NAME;
 
 	int len = strlen(str);
-	int f[MAX][MAX];
+	int d[MAX][MAX];
 
 	for(int i = 0; i <= len; i++)
 	{
-		f[i][i] = 0;
-		f[i+1][i] = 0;
+		d[i][i] = 0;
+		d[i+1][i] = 0;
 	}
 
 	for(int t = 1; t < len; t++)
@@ -209,37 +210,80 @@ void create_palindrome(char str[])
 		for(int i = 0, j = t; j < len; i++,j++)
 		{
 			if(str[i] == str[j])
-				f[i][j] = f[i+1][j-1];
+				d[i][j] = d[i+1][j-1];
 			else
-				f[i][j] = 1 + min(f[i+1][j], f[i][j-1]);
+				d[i][j] = 1 + min(d[i+1][j], d[i][j-1]);
 		}
 	}
 
-	PRINT_MATRIX(f, len, len);
-//	cout<<f[0][len-1]<<endl;
+	PRINT_MATRIX(d, len, len);
+//	cout<<d[0][len-1]<<endl;
 }
 
 void test_create_palindrome()
 {
 	create_palindrome("Ab3bd");
 }
+
+/**********************************************
+	子集和(subset sum)
+	d[i][j] = true : a[0]~a[i-1]存在几个元素能组合得到k
+		case1:不包含a[i-1]元素，则d[i][j] == d[i-1][j]
+		case2:包含a[i-1]元素，则d[i][j] == d[i-1][j-a[i-1]]
+
+**********************************************/
+void subset_sum(int a[], int n, int k)
+{
+	PRINT_SUB_FUNCTION_NAME;
+
+	bool d[MAX][MAX];
+
+	for(int i = 0; i <= n; i++)
+		d[i][0] = true;
+	for(int j = 1; j <= k; j++)	// 注意：j从1开始置false，不要写成j=0
+		d[0][j] = false;
+
+	for(int i = 1; i <= n; i++)
+	{
+		for(int j = 1; j <= k; j++)
+		{
+			if(a[i-1] > j)		// a[i-1]已大于目标j，直接舍弃
+				d[i][j] = d[i-1][j];
+			if(a[i-1] <= j)		// a[i-1]不大于目标j，舍弃or包含a[i-1]元素
+				d[i][j] = d[i-1][j] || d[i-1][j-a[i-1]];
+		}
+	}
+
+#ifdef DEBUG_LOG
+	PRINT_MATRIX(d, n+1, k+1);
+#endif
+
+	cout<<d[n][k]<<endl;
+}
+
+void test_subset_sum()
+{
+	int a[] = {8,2,4};
+	int k = 10;
+	subset_sum(a, Len(a), k);
+}
 /**********************************************
 	4.01背包
 **********************************************/
-void packing_01_result(int f[MAX][MAX], int w[], int v[], int n, int c)	// n = item_num, c = capacity
+void packing_01_result(int d[MAX][MAX], int w[], int v[], int n, int c)	// n = item_num, c = capacity
 {
 	if(n < 0)
 		return;
 
-	if(f[n][c] == f[n-1][c])
-		packing_01_result(f, w, v, n-1, c);
+	if(d[n][c] == d[n-1][c])
+		packing_01_result(d, w, v, n-1, c);
 	else
 	{
 		if(w[n] <= c 
-		&& f[n][c] == v[n] + f[n-1][c-w[n]])
+		&& d[n][c] == v[n] + d[n-1][c-w[n]])
 		{	
 			cout<<"物品"<<n<<", 剩余空间"<<c-w[n]<<endl;
-			packing_01_result(f, w, v, n-1, c-w[n]);
+			packing_01_result(d, w, v, n-1, c-w[n]);
 		}
 	}
 }
@@ -248,41 +292,41 @@ void packing_01(int w[], int v[], int item_num, int capacity)
 {
 	PRINT_SUB_FUNCTION_NAME;
 
-	int f[MAX][MAX]; bzero(f, sizeof(f));
+	int d[MAX][MAX]; bzero(d, sizeof(d));
 	
 	for(int i = 1; i <= item_num; i++)
 		for(int j = 1; j <= capacity; j++)
 			if(w[i] <= j)	// 当前物品i的重量w[i]小于背包容量j，可以放入
-				f[i][j] = max(f[i-1][j],f[i-1][j-w[i]]+v[i]);
+				d[i][j] = max(d[i-1][j],d[i-1][j-w[i]]+v[i]);
 			else			// 当前背包放不下物品i
-				f[i][j] = f[i-1][j];
+				d[i][j] = d[i-1][j];
 
 #ifdef DEBUG_LOG	
-	PRINT_MATRIX(f, item_num+1, capacity+1);
+	PRINT_MATRIX(d, item_num+1, capacity+1);
 #endif
 
-	cout<<"f["<<item_num<<"]["<<capacity<<"] = "<<f[item_num][capacity]<<", 包含:"<<endl;
-	packing_01_result(f, w,v,item_num,capacity);
+	cout<<"d["<<item_num<<"]["<<capacity<<"] = "<<d[item_num][capacity]<<", 包含:"<<endl;
+	packing_01_result(d, w,v,item_num,capacity);
 }
 
 void packing_01_improve(int w[], int v[], int item_num, int capacity)
 {
 	PRINT_SUB_FUNCTION_NAME;
 
-	int f[MAX]; bzero(f, sizeof(f));
+	int d[MAX]; bzero(d, sizeof(d));
 
 	for(int i = 1; i <= item_num; i++)
 	{
 		for(int j = capacity; j > 0; j--)
 			if(w[i] <= j)	// 当前物品i的重量w[i]小于背包容量j，可以放入
-				f[j] = max(f[j], f[j-w[i]]+v[i]);
+				d[j] = max(d[j], d[j-w[i]]+v[i]);
 
 #ifdef DEBUG_LOG
-		PRINT_ARRAY(f, capacity+1);
+		PRINT_ARRAY(d, capacity+1);
 #endif
 	}
 
-	cout<<"f["<<capacity<<"] = "<<f[capacity]<<endl;
+	cout<<"d["<<capacity<<"] = "<<d[capacity]<<endl;
 }
 
 void test_package()
@@ -301,6 +345,7 @@ int main()
 	test_1_dim();
 	test_2_dim();
 	test_create_palindrome();
+	test_subset_sum();
 	test_package();
 
 	return 0;
--- titech/28.cpp
+++ titech/28.cpp
@@ -15,15 +15,9 @@ using namespace std;
 int f(int a[], int n, int k, int s, int i)
 {
 	if(i == n)
-	{
-		cout<<"----"<<endl;
 		return s == k;
-	}
 	else
-	{
-		cout<<"++++"<<endl;
 		return f(a, n, k, s, i+1) || f(a, n, k, s+a[i], i+1);
-	}
 }
 
 void g(int a[], int n, int k)
@@ -38,7 +32,7 @@ void g(int a[], int n, int k)
 			b[i][j] = 0;
 		for(j = 0; j <= k; j++)
 		{
-			if(k == a[j])
+			if(j == 0)
 				b[i][j] = 1;
 			if(i > 0)
 			{
@@ -60,12 +54,54 @@ void g(int a[], int n, int k)
 	cout<<"ret = "<<b[n-1][k]<<endl;
 }
 
+bool isSubsetSum(int set[], int n, int sum)
+{
+	if (sum == 0)
+		return true;
+	if (n == 0 && sum != 0)
+		return false;
+	if (set[n-1] > sum)
+		return isSubsetSum(set, n-1, sum);
+	return isSubsetSum(set, n-1, sum) || isSubsetSum(set, n-1, sum-set[n-1]);
+}
+
+bool dp_isSubsetSum(int set[], int n, int sum)
+{
+    bool subset[n+1][sum+1];
+
+    for (int i = 0; i <= n; i++)
+      subset[i][0] = true;
+    for (int i = 1; i <= sum; i++)
+      subset[0][i] = false;
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= sum; j++)
+		{
+			if(j<set[i-1])
+				subset[i][j] = subset[i-1][j];
+			if (j >= set[i-1])
+				subset[i][j] = subset[i-1][j] || subset[i - 1][j-set[i-1]];
+		}
+	}
+  
+	for(int i = 0; i <= n; i++)
+	{
+		for(int j = 0; j <= sum; j++)
+			cout<<subset[i][j]<<" ";
+		cout<<endl;
+	}
+  
+	return subset[n][sum];
+}
+
 void test_1()
 {
-	int a[] = {3,7,5,8,2};
-	int k = 11;
-//	cout<< f(a, Len(a), k, 0, 0);
-	g(a, Len(a), k);
+	int a[] = {8,2,4};
+	int k = 10;
+//	cout<<f(a, Len(a), k, 0, 0)<<endl;
+	cout<<dp_isSubsetSum(a, Len(a), k)<<endl;
+//	g(a, Len(a), k);
 }
 
 int main()
