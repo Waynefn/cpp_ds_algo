c72591369d22bc7f9df4534aeef4dc890a5b3550
we333 <wantone321@gmail.com>
2018-02-20 21:56:31 +0900

    lis by N*logN OK

--- algo/utils.h
+++ algo/utils.h
@@ -9,4 +9,5 @@ using namespace std;
 #define PRINT_FUNCTION_NAME	{cout<<endl<<"********** "<<__FUNCTION__<<" **********"<<endl;	}
 #define PRINT_SUB_FUNCTION_NAME	{cout<<endl<<"->"<<__FUNCTION__<<endl;}
 #define PRINT_ARRAY(a,n){for(int i = 0; i < n; i++) cout<<a[i]<<"|"; cout<<endl;}
+#define PRINT_ARRAY_2(a, s, e){for(int i = s; i < e; i++) cout<<a[i]<<"|"; cout<<endl;}
 #define PRINT_MATRIX(a,m,n){for(int i = 0; i < m; i++){for(int j = 0; j < n; j++) cout<<a[i][j]<<"|"; cout<<endl;}}
